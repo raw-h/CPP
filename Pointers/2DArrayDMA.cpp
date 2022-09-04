f80558c72297ae0763c6debeeb5f081aba180120
raw-h <rawat.shubham2400@gmail.com>
2022-09-04 12:01:10 +0530

    2D Array DMA

---
+++ Pointers/2DArrayDMA.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m, n;
+    cin >> m >> n;
+    int * arr = new int[m * n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> *(arr + (i * n) + j);
+    
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++)
+            cout << *(arr + (i * n) + j) << " ";
+        cout << endl;
+    }
+    delete [] arr;
+    return 0;
+}
