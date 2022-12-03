ca55fbeb05a5f6e9422c7c88a89fbc91e89da60e
raw-h <rawat.shubham2400@gmail.com>
2022-12-03 20:23:18 +0530

    Lexicographical Question

---
+++ Maps/Questions.cpp
@@ -0,0 +1,21 @@
+/*
+Given N strings, print unique strings in lexicographical order with their frequency.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    map<string,int> q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string a;
+        cin >> a;
+        // q[a] = q[a] + 1;
+        q[a]++;
+    }
+    for(auto pt: q)
+        cout << pt.first << " " << pt.second << endl;
+    return 0;
+}
