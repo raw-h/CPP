e7e4ec661a680e3ee3f7b72483ecb83178674103
raw-h <rawat.shubham2400@gmail.com>
2023-03-29 11:32:22 +0530

    19

---
+++ Patterns/LB/19.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int temp = i;
+        int j = 1;
+        while(j <= (n-i)){
+            cout << " ";
+            j++;
+        }
+        while(temp){
+            cout << "*";
+            temp--;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
