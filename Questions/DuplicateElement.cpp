384dfb3265d7de056a476017a56b51e08c389816
raw-h <rawat.shubham2400@gmail.com>
2023-04-04 05:53:29 +0530

    Duplicate Element

---
+++ Questions/DuplicateElement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    int ans = 0;
+    for(int i = 0; i < arr.size(); i++){
+        ans = ans ^ arr[i];
+    }
+    for(int i = 1; i < arr.size(); i++){
+        ans = ans ^ i;
+    }
+    cout << ans << endl;
+    return 0;
+}
