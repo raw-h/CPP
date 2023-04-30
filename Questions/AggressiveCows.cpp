5644705ebc7f7a410601ad65fb02a8f5751db97f
raw-h <rawat.shubham2400@gmail.com>
2023-04-30 11:53:21 +0530

    Aggressive Cows

--- Questions/AggressiveCows.cpp
+++ Questions/AggressiveCows.cpp
@@ -1,50 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool isPossibleToPlace(vector<int> stalls, int cows, int gap){
+    int cowCount = 1;
+    int lastPosition = stalls[0];
+    for(int i = 0; i < stalls.size(); i++){
+        if(stalls[i] - lastPosition >= gap){
+            cowCount++;
+            if(cowCount == cows)
+                return true;
+            lastPosition = stalls[i];
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> stalls, int k)
+{
+    int start = 0;
+    int end = -1;
+    for (int i = 0; i < stalls.size(); i++)
+        end = max(end, stalls[i]);
+
+    int mid;
+    int ans;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if(isPossibleToPlace(stalls, k, mid)){
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+            end = mid - 1;
+    }
+    return ans;
+}
+
 int main()
 {
     int n;
     cin >> n;
-    int arr[n];
-    int mini = INT_MAX, maxi = INT_MIN;
+    vector<int> stalls;
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
-        if (arr[i] < mini)
-            mini = arr[i];
-        if (arr[i] > maxi)
-            maxi = arr[i];
+        int x;
+        cin >> x;
+        stalls.push_back(x);
     }
     int cows;
     cin >> cows;
-    int space = maxi - mini;
-    int start = 0, end = space, mid;
-    sort(arr, arr + n);
-    int final_ans;
-    bool flag = 0;
-    while (start <= end)
-    {
-        mid = start + (end - start) / 2;
-        int cow_count = 1;
-        int last_pos = arr[0];
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] - last_pos >= mid)
-            {
-                cow_count++;
-                final_ans = mid;
-                if (cow_count == cows)
-                {
-                    flag = 1;
-                    break;
-                }
-                last_pos = arr[i];
-            }
-        }
-        if(flag)
-            start = mid + 1;
-        else
-            end = mid - 1;
-    }
-    cout << final_ans << endl;
+    int answer = aggressiveCows(stalls, cows);
+    cout << answer << endl;
     return 0;
 }
