9d8c3033bc133f43ac8a194e0cec83029f16d434
raw-h <rawat.shubham2400@gmail.com>
2023-07-17 10:16:01 +0530

    Bubble Sort (Recurcise)

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int *arr, int size){
+    if(size == 0 || size == 1)
+        return ;
+    for(int i = 0; i < size - 1; i++){
+        if(arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+    }
+    bubble_sort(arr, size - 1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
