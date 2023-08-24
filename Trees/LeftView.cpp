d1f79461f0763cad18d394430ccd2fba539c67d5
raw-h <rawat.shubham2400@gmail.com>
2023-08-24 11:38:01 +0530

    Left View

---
+++ Trees/LeftView.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void solve(Node *root, vector<int> &ans, int level)
+{
+    if (root == NULL)
+        return;
+
+    if (level == ans.size())
+        ans.push_back(root->data);
+
+    solve(root->left, ans, level + 1);
+    solve(root->right, ans, level + 1);
+}
+
+vector<int> leftView(Node *root)
+{
+    vector<int> ans;
+    solve(root, ans, 0);
+    return ans;
+}
+
+int main(){
+    return 0;
+}
